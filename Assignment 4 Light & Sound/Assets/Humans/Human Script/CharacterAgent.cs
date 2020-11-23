8e284367e8897f6bf44c5286b4ce36e9d84ce2dc
Chambers <964089@swansea.ac.uk>
2020-11-23 23:34:56 +0000

    Added code to the human AI, he character walks around and changes direction using a destination trigger, the turning could be smoother and more refined but the character is moving around avoiding obsticlses which is a major plus.

--- Assignment 4 Light & Sound/Assets/Humans/Human Script/CharacterAgent.cs
+++ Assignment 4 Light & Sound/Assets/Humans/Human Script/CharacterAgent.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
-public class Characteragent : MonoBehaviour
+public class CharacterAgent : MonoBehaviour
 {
+
+    public GameObject characterDestination;
+    NavMeshAgent theAgent; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        theAgent.SetDestination(characterDestination.transform.position);
     }
 }
---
+++ Assignment 4 Light & Sound/Assets/Humans/Human Script/DestinationChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationChange : MonoBehaviour
+{
+    public int xPos;
+    public int zPos;
+
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Character")
+        {
+            xPos = Random.Range(6, 14);
+            zPos = Random.Range(-14, -3);
+            this.gameObject.transform.position = new Vector3(xPos, 4.0039f, zPos); 
+         }
+    }
+}
