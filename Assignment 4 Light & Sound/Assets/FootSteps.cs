78edce53622dbd4a957d80f458fef1aca300c727
Chambers <964089@swansea.ac.uk>
2020-11-27 05:07:07 +0000

    Have added sound and managed to have an interactable sound feature that adds to the room. The sounds i've added include footsteps on the NPC and a Orb sound i made using huming sounds. Just need to add one more NPC with sound.

---
+++ Assignment 4 Light & Sound/Assets/FootSteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    AudioSource animationSoundPlayer;
+
+
+  
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animationSoundPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void PlayerFootstepSound()
+    {
+        animationSoundPlayer.Play();
+    }
+}
